ec191e7a6f7ef98da392e0c2a9d5c3be092d2f65
Sicelo Mduna <siceloit@gmail.com>
2025-10-23 18:12:13 +0200

    save

---
+++ front_end/app/pages/JobSeeker/JobDetials.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JobDetials = () => {
+  return (
+    <div>JobDetials</div>
+  )
+}
+
+export default JobDetials
--- front_end/app/routes/pages/notFound.tsx
+++ front_end/app/routes/pages/notFound.tsx
@@ -1,3 +1,9 @@
-export default function NotFound() {
-  return <h1>404 - Page Not Found</h1>;
+import React from 'react'
+
+const notFound = () => {
+  return (
+    <h1>404 - Page Not Found</h1>
+  )
 }
+
+export default notFound
